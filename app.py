0cb8dcfc70f3dc333dec721bb7cd271004a9a4ea
OnurUsll <118818756+OnurUsll@users.noreply.github.com>
2025-05-20 15:53:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ with center:
             st.stop()
 
         st.subheader("Prediction Result")
-        if prediction[0] == 1:
+        if prediction[0] > 0.5:
             st.success("✅ The client is likely to subscribe to the term deposit!")
         else:
             st.error("❌ The client is unlikely to subscribe to the term deposit.")
